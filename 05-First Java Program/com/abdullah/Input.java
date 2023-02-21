d23d1bdb572689f335ba37bdaa7e0ba041be2c0b
Abdullah-9862873 <82256525+Abdullah-9862873@users.noreply.github.com>
2023-02-21 21:29:51 +0500

    Day-3

---
+++ 05-First Java Program/com/abdullah/Input.java
@@ -0,0 +1,37 @@
+package com.abdullah;
+
+// import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        // System.out.println("Enter the value: ");
+        // Scanner input = new Scanner(System.in);
+        // int value1 = input.nextInt();
+        // input.close();
+        // NumberFormat nf = NumberFormat.getInstance();
+        // String formattedValue = nf.format(value1);
+        // System.out.println("The value you entered is: " + formattedValue);
+
+        // Scanner input = new Scanner(System.in);
+        // float value1 = input.nextFloat();
+        // input.close();
+        // System.out.println("The value you entered was: " + value1);
+
+        // Scanner input = new Scanner(System.in);
+        // double value2 = input.nextDouble();
+        // input.close();
+        // System.out.println("The value you entered was: " + value2);
+
+        // Scanner input = new Scanner(System.in);
+        // long value3 = input.nextLong();
+        // input.close();
+        // System.out.println("The value you entered is: " + value3);
+
+        // Scanner input = new Scanner(System.in);
+        // boolean value4 = input.nextBoolean();
+        // input.close();
+        // System.out.println("The boolean value you entered is: " + value4);
+
+    }
+}
---
+++ 05-First Java Program/com/abdullah/Primitives.java
@@ -0,0 +1,32 @@
+package com.abdullah;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollNo = 64;
+        char letter = 'H';
+        float marks = 98.35f;
+        double largeDecimalNumbers = 2342.2342324;
+        long largeIntegers = 12341234124L;
+        boolean check = true;
+
+    }
+}
+
+/*
+ * <-----------------------------Primitve_Data_Types------------------------->
+ * Primitive datatypes are the datatypes that you cannot break further
+ * 
+ * 
+ * int -------------------> 4 bytes
+ * char -------------------> 1 bytes
+ * float --------------> 4 bytes ------------> Store upto 7 decimal digits
+ * double ---------------> 8 bytes -----------> Store upto 15 decimal digits
+ * long -------------------> 8 bytes -----------------> Store large integers
+ * boolean -------------------> 2 bytes
+ * 
+ * By default the decimal numbers are stored in "double" and to convert them to
+ * "float" we have to use the "f"
+ * 
+ * By default the value is "integer" and to convert it to store large values we
+ * use long and use the variable "L"
+ */

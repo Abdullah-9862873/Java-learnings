b7e35fd817451cd281f8f9bfc730e7c077f1e355
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2023-04-25 16:43:46 +0500

    Update SubArray.java

--- 29-Solve Any Recursion Subset Subsequence String Question Part 1/SubArray.java
+++ 29-Solve Any Recursion Subset Subsequence String Question Part 1/SubArray.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class SubArray {
     public static void main(String[] args) {
-        int[] arr = {1,2,2};
-        List<List<Integer>> ans = findSubArray2(arr);
+        int[] arr = {1,2,2,2,2,2};
+        List<List<Integer>> ans = findSubArray3(arr);
         for(List<Integer> list: ans){
             System.out.println(list);
         }
@@ -24,7 +24,6 @@ public class SubArray {
                 outer.add(internal);
             }
         }
-
         return outer;
     }
 
@@ -38,6 +37,26 @@ public class SubArray {
         for(int i=0; i<arr.length; i++){
             int size = outer.size();
             startIndex = (i>0 && arr[i] == arr[i-1]) ? size/2 : 0;
+            for(int j=startIndex; j<size; j++){
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+
+    // Here the findSubArray2 has a problem that if you pass the array like [1,2,2,2] then it won't work well so in order tol solve it we can do something like
+    static List<List<Integer>> findSubArray3(int[] arr){
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+
+        outer.add(new ArrayList<>());
+        int startIndex = 0;
+
+        for(int i=0; i<arr.length; i++){
+            int size = outer.size();
+            startIndex = (i>0 && arr[i] == arr[i-1]) ? size-2 : 0;
             for(int j=startIndex; j<size; j++){
                 List<Integer> internal = new ArrayList<>(outer.get(j));
                 internal.add(arr[i]);

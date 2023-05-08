96463964e99e61fbbbc1b4ae79f7220ba17e1c96
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2023-05-08 11:09:08 +0500

    initial commit

---
+++ 44-Stacks & Queues Interview Questions - Google, Facebook, Amazon, Microsoft/Intro.java
@@ -0,0 +1,7 @@
+/*
+The answer to the question that when to use Stacks and Queues is that:
+-----> When you see an order in the question
+-----> Or you see something like I have to store values from here till here
+-----> Holding values for some time, working with orders and sequences
+Then there is a high probability that Stacks or Queues are gonna be used in that question
+*/

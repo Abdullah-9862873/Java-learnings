03e2b7772ed76fc6120bc6a4d541f0f7592e0905
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2023-05-15 10:19:07 +0500

    Delete Graph/Edge List directory

--- Graph/Edge List/Intro.java
+++
@@ -1,8 +0,0 @@
-/*
-_____________________________________________________________________________________________________________
-----> In edge list, a single list contains the information of all the edges... It is represented something like the following:
-Edge list = {{0,2},{2,3},{1,3},{1,2}}
-
----> It is used in the questions which required sorting the edges based on the destination or weight like Minimum Spanning Tree question
-_____________________________________________________________________________________________________________
- */

61073cdfeb67975e32d8962a018e08775694f44c
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2023-06-27 15:21:02 +0500

    Update BinarySearchIntro.java

--- 11-Binary Search Theory Implementation/com/main/BinarySearchIntro.java
+++ 11-Binary Search Theory Implementation/com/main/BinarySearchIntro.java
@@ -1,5 +1,6 @@
 package com.main;
 
+// If the array is sorted then it must click on the mind to use binary search
 public class BinarySearchIntro {
     public static void main(String[] args) {
         int[] arr = { 2, 4, 6, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18 };
@@ -14,7 +15,7 @@ public class BinarySearchIntro {
         int end = arr.length - 1;
 
         while (start <= end) {
-            int mid = start + (end - start) / 2;
+            int mid = start + (end - start) / 2;    // If we do mid = (start + end)/2 ----> Then lets say we have a very large start and end.... In that case the start + end will exceed the range of int... So a better way to do this is (start + (end - start) / 2)
 
             if (target < arr[mid]) {
                 end = mid - 1;

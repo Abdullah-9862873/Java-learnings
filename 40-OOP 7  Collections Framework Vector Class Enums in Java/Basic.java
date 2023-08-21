dcfc1218ed1567f0c509380b6d2424e0b74efa89
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2023-08-21 14:02:45 +0500

    Delete 40-OOP 7  Collections Framework Vector Class Enums in Java directory

--- 40-OOP 7  Collections Framework Vector Class Enums in Java/Basic.java
+++
@@ -1,7 +0,0 @@
-/* 
-_________________________________________________________________________________
-----> In java there are so many data structures that share some common methods or fetures like add() get() remove() and all... So java members decided to make a collection of these methods and stuff and place it in an interface called "Collection" and now the data structures that uses these basically implements from the "Collection" interface and the internal impleentation of these depends on the type of data structure... Obviously, this is made an abstraction from the user so we don't really care about the implementation... Java Collection Framework picture is in this folder
-
-_________________________________________________________________________________
-----> Enums are basically public, static and final constants type that are fixed and that cannot be changed or reinstantiated... So basically they are used when we know that only these much variables can exist in the environment... For Example we know that the week days cannot exceed seven... So we create an enum of Week and list all the seven days inside...
-*/
